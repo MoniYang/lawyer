e156074ff8bdac2265980939fd93aa22ef28aa77
moni.yang <moni.yang@onelab.tw>
2022-10-15 00:09:12 +0800

    1015修改

--- app/public/template/js/demo.js
+++ app/public/template/js/demo.js
@@ -1,15 +1,18 @@
 
 
-$(".dropdown").on("click", function (e) {
+// $(".dropdown").on("hover", function (e) {
+   
+// });
+$(".dropdown").hover(function() {
     console.log(1);
     $(this).attr("data-open") == "true" ? $(this).attr("data-open","false") : $(this).attr("data-open", "true");
     e.stopPropagation();
 });
 
-$("body").on("click", function () {
+// $("body").on("click", function () {
  
-    $('.dropdown').attr("data-open","false");
-});
+//     $('.dropdown').attr("data-open","false");
+// });
 
 
 var swiper = new Swiper(".new-download__swiper .swiper", {
