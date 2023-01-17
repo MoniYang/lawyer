15c91b0f8482b3c2b1085f94efe0f857c7293ce6
moni.yang <moni.yang@onelab.tw>
2023-01-17 18:43:06 +0800

    [修改]修改member 側邊選單

--- app/public/template/js/demo.js
+++ app/public/template/js/demo.js
@@ -8,6 +8,7 @@ $('.header__nav[role="mobile-visible"] .dropdown').on("click", function (e) {
     $(this).attr("data-open") == "true" ? $(this).attr("data-open","false") : $(this).attr("data-open", "true");
     e.stopPropagation();
 });
+
 $('.header__nav[role="desktop-visible"] .dropdown').hover(function(e) {
 
     $(this).attr("data-open") == "true" ? $(this).attr("data-open","false") : $(this).attr("data-open", "true");
@@ -61,6 +62,11 @@ $(".header__main .btn--nav").on("click", function (e) {
     $(".header__nav").attr("data-open","true");
     e.stopPropagation();
 });
+
+$("input#triple").on("click", function (e) {
+    $(".mm-info--triple").attr("data-open","true");
+});
+
 var swiper = new Swiper(".new-download__swiper .swiper", {
     
     autoplay: {
